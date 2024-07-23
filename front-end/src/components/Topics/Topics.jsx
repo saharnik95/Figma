8989ca8decf733df1d477430ff8f6d8d306f4da4
sahar <saharnikkhah81@gmail.com>
2024-07-23 15:05:32 +0430

    first commit

---
+++ front-end/src/components/Topics/Topics.jsx
@@ -0,0 +1,38 @@
+import "./topics.css"
+import {
+  frequencies,
+  daw,
+  vocalsProcessing,
+  mixing,
+  mixingConsole,
+  mastering,
+  bgElement1
+} from "../../assets";
+import { useState } from "react";
+export const Topics = () => {
+  const [currentImage,setCurrentImage]=useState(frequencies);
+  return (
+    <section id="topics" className="black">
+      <div className="wrapper">
+        <h2> What will you learn?</h2>
+
+          
+          <div className="content-container">
+            <ul className="topics-list">
+              <li onMouseEnter={()=>setCurrentImage(frequencies)}>What are frequencies?</li>
+              <li onMouseEnter={()=>setCurrentImage(daw)}>Using DAW</li>
+              <li onMouseEnter={()=>setCurrentImage(vocalsProcessing)}>Vocals Processing</li>
+              <li onMouseEnter={()=>setCurrentImage(mixing)}>Mixing</li>
+              <li onMouseEnter={()=>setCurrentImage(mixingConsole)}>Mixing Console</li>
+              <li onMouseEnter={()=>setCurrentImage(mastering)}>Mastering</li>
+            </ul>
+            <div className="topic-image">
+              <img src={currentImage} alt="" />
+            </div>
+          </div>
+        <img src={bgElement1} className="bg-element1"/> 
+      </div>
+    </section>
+  )
+}
+export default Topics;
